f1ed6414b4ad9f45beffebf1ec2f02876ef553cc
Eui9179 <outdark97@gmail.com>
2023-03-20 16:24:12 +0900

    answer 생성 기능 추가

---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = Answer.builder()
+                .content(content)
+                .createDate(LocalDateTime.now())
+                .question(question)
+                .build();
+        answerRepository.save(answer);
+    }
+
+}
